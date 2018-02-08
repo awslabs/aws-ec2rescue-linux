0a2bd91e36808a1ed1c912bab4eaebe2baa91f84
Sean Poynter <seanpoyn@amazon.com>
2018-02-08 15:01:07 -0600

    Refactor udevpersistentnet module.

--- docs/modules/udevpersistentnet.md
+++ docs/modules/udevpersistentnet.md
@@ -4,8 +4,7 @@
 Incorrect rules in your /etc/udev/rules.d/70-persistent-net.rules file can result in interfaces not coming up with the proper interface name, or at all.
 
 ## Resolving with EC2 Rescue for Linux
-EC2 Rescue for Linux will automatically move the rules to a backed up location, which will result in proper rules being generated upon reboot.
-
+EC2 Rescue for Linux will automatically comment out all lines in the rules file.
 ```commandline
 $ sudo ./ec2rl run --only-modules=udevpersistentnet --remediate --udevpersistentnet
 ```
@@ -13,13 +12,26 @@ $ sudo ./ec2rl run --only-modules=udevpersistentnet --remediate --udevpersistent
 Passing output:
 ```commandline
 ----------[Diagnostic Results]----------
-module run/udevpersistentnet       [SUCCESS] Moved /etc/udev/rules.d/70-persistent-net.rules to /etc/udev/rules.d/70-persistent-net.bak
+module run/udevpersistentnet       [SUCCESS] commented out the lines in /etc/udev/rules.d/70-persistent-net.rules
+```
+
+Failing output:
+
+```commandline
+----------[Diagnostic Results]----------
+module run/arpignore       [FAILURE] failed to comment out the lines in /etc/udev/rules.d/70-persistent-net.rules
 ```
 
 ## Resolving Manually
 
-Move the file to a different location or delete it.
+Move the file to a different location, delete it, or edit the file and comment out the rules.
 
 ```commandline
 $ sudo mv /etc/udev/rules.d/70-persistent-net.rules /etc/udev/rules.d/70-persistent-net.bak
+```
+```commandline
+$ sudo rm /etc/udev/rules.d/70-persistent-net.rules
+```
+```commandline
+$ sudo vim /etc/udev/rules.d/70-persistent-net.rules
 ```
--- tools/moduletests/unit/test_udevpersistentnet.py
+++ tools/moduletests/unit/test_udevpersistentnet.py
@@ -15,7 +15,6 @@
 Unit tests for the udevpersistentnet module
 """
 import os
-import subprocess
 import sys
 import unittest
 
@@ -37,14 +36,9 @@ else:
     # contextlib2 is a backport of contextlib from Python 3.5 and is compatible with Python2/3
     import contextlib2 as contextlib
 
-# builtins was named __builtin__ in Python 2 so accommodate the change for the purposes of mocking the open call
-if sys.version_info >= (3,):
-    builtins_name = "builtins"
-else:
-    builtins_name = "__builtin__"
-
 
 class Testudevpersistentnet(unittest.TestCase):
+    rules_file = "/etc/udev/rules.d/70-persistent-net.rules"
 
     def setUp(self):
         self.output = StringIO()
@@ -54,31 +48,133 @@ class Testudevpersistentnet(unittest.TestCase):
 
     @mock.patch("moduletests.src.udevpersistentnet.os.path.isfile", return_value=False)
     def test_detect_noproblem(self, isfile_mock):
-        with self.assertRaises(SystemExit) as ex:
-            moduletests.src.udevpersistentnet.detect()
-        self.assertEqual(ex.exception.code, 0)
+        self.assertFalse(moduletests.src.udevpersistentnet.detect(self.rules_file))
+        self.assertTrue(isfile_mock.called)
 
     @mock.patch("moduletests.src.udevpersistentnet.os.path.isfile", return_value=True)
     def test_detect_problem(self, isfile_mock):
-        self.assertTrue(moduletests.src.udevpersistentnet.detect())
+        self.assertTrue(moduletests.src.udevpersistentnet.detect(self.rules_file))
+        self.assertTrue(isfile_mock.called)
 
-    @mock.patch("moduletests.src.udevpersistentnet.os.rename", return_value=True)
-    def test_fix_success(self, rename_mock):
-        self.assertTrue(moduletests.src.udevpersistentnet.fix())
+    @mock.patch("os.chown", return_value=True)
+    @mock.patch("os.chmod", return_value=True)
+    @mock.patch("shutil.copy2", return_value=True)
+    def test_fix_success(self, copy2_mock, os_chmod_mock, os_chown_mock):
+        # A comment, a valid line, and a line consisting of whitespace
+        line = "# a comment\n" \
+               "SUBSYSTEM==\"net\", " \
+               "ACTION==\"add\", " \
+               "DRIVERS==\"?*\", " \
+               "ATTR{address}==\"0e:1e:4f:fd:a1:2c\", " \
+               "NAME=\"eth0\"\n" \
+               "\t\n"
+        open_mock = mock.mock_open(read_data=line)
+        # mock_open does not have support for iteration so it must be added manually
+        # readline() until a blank line is reached (the sentinel)
 
-    @mock.patch("moduletests.src.udevpersistentnet.os.path.isfile", return_value=False)
-    def test_fix_failure(self, isfile_mock):
-        with self.assertRaises(SystemExit) as ex:
-            moduletests.src.udevpersistentnet.fix()
-        self.assertEqual(ex.exception.code, 0)
+        def iter_func(self):
+            return iter(self.readline, "")
 
+        open_mock.return_value.__iter__ = iter_func
+
+        def py3_next_func(self):
+            return next(iter(self.readline, ""))
+
+        if sys.hexversion >= 0x3000000:
+            open_mock.return_value.__next__ = py3_next_func
+
+        # noinspection PyUnresolvedReferences
+        with mock.patch.object(moduletests.src.udevpersistentnet.tempfile, "NamedTemporaryFile") as temp_file_mock:
+            with mock.patch("moduletests.src.udevpersistentnet.open", open_mock):
+                with contextlib.redirect_stdout(StringIO()):
+                    self.assertTrue(moduletests.src.udevpersistentnet.fix(self.rules_file))
+                self.assertTrue(temp_file_mock.called)
+                self.assertEqual(str(temp_file_mock.mock_calls),
+                                 '[call(mode=\'wt\'),\n call().__enter__(),\n '
+                                 'call().__enter__().write(\'# a comment\\n\'),\n '
+                                 'call().__enter__().write(\'# SUBSYSTEM=="net", '
+                                 'ACTION=="add", DRIVERS=="?*", '
+                                 'ATTR{address}=="0e:1e:4f:fd:a1:2c", '
+                                 'NAME="eth0" # commented out by ec2rl\\n\'),\n '
+                                 'call().__enter__().write(\'\\t\\n\'),\n '
+                                 'call().__enter__().flush(),\n '
+                                 'call().__exit__(None, None, None)]')
+                self.assertTrue(open_mock.called)
+                self.assertTrue(copy2_mock.called)
+                self.assertTrue(os_chmod_mock.called)
+                self.assertTrue(os_chown_mock.called)
+
+    @mock.patch("moduletests.src.udevpersistentnet.get_config_dict")
     @mock.patch("moduletests.src.udevpersistentnet.detect", return_value=True)
+    @mock.patch("moduletests.src.udevpersistentnet.backup", return_value=True)
     @mock.patch("moduletests.src.udevpersistentnet.fix", return_value=True)
-    def test_run_success(self, detect_mock, fix_mock):
-        self.assertTrue(moduletests.src.udevpersistentnet.run())
+    def test_run_success(self, fix_mock, detect_mock, backup_mock, get_config_dict_mock):
+        get_config_dict_mock.return_value = {"BACKUP_DIR": "/var/tmp/ec2rl",
+                                             "LOG_DIR": "/var/tmp/ec2rl",
+                                             "BACKED_FILES": dict(),
+                                             "REMEDIATE": True}
+        with contextlib.redirect_stdout(self.output):
+            self.assertTrue(moduletests.src.udevpersistentnet.run())
+        self.assertTrue(self.output.getvalue().endswith("/docs/modules/udevpersistentnet.md for further details\n"))
+        self.assertTrue(detect_mock.called)
+        self.assertTrue(fix_mock.called)
+        self.assertTrue(backup_mock.called)
+        self.assertTrue(get_config_dict_mock.called)
 
-    @mock.patch("moduletests.src.udevpersistentnet.detect", side_effect=Exception)
-    def test_run_failure(self, detect_mock):
-        with self.assertRaises(SystemExit) as ex:
-            self.assertRaises(Exception, moduletests.src.udevpersistentnet.run())
-        self.assertEqual(ex.exception.code, 0)
+    @mock.patch("moduletests.src.udevpersistentnet.get_config_dict")
+    @mock.patch("moduletests.src.udevpersistentnet.detect", return_value=True)
+    @mock.patch("moduletests.src.udevpersistentnet.backup", return_value=True)
+    @mock.patch("moduletests.src.udevpersistentnet.fix", return_value=False)
+    def test_run_success_failure(self, fix_mock, backup_mock, detect_mock, get_config_dict_mock):
+        get_config_dict_mock.return_value = {"BACKUP_DIR": "/var/tmp/ec2rl",
+                                             "LOG_DIR": "/var/tmp/ec2rl",
+                                             "BACKED_FILES": dict(),
+                                             "REMEDIATE": True}
+        with contextlib.redirect_stdout(self.output):
+            self.assertFalse(moduletests.src.udevpersistentnet.run())
+        self.assertTrue("[FAILURE] failed to comment out the lines in /etc/udev/rules.d/70-persistent-net.rules"
+                        in self.output.getvalue())
+        self.assertTrue(fix_mock.called)
+        self.assertTrue(backup_mock.called)
+        self.assertTrue(detect_mock.called)
+        self.assertTrue(get_config_dict_mock.called)
+
+    @mock.patch("moduletests.src.udevpersistentnet.get_config_dict")
+    @mock.patch("moduletests.src.udevpersistentnet.detect", return_value=False)
+    def test_run_no_rule_file(self, detect_mock, get_config_dict_mock):
+        get_config_dict_mock.return_value = {"BACKUP_DIR": "/var/tmp/ec2rl",
+                                             "LOG_DIR": "/var/tmp/ec2rl",
+                                             "BACKED_FILES": dict(),
+                                             "REMEDIATE": True}
+        with contextlib.redirect_stdout(self.output):
+            self.assertTrue(moduletests.src.udevpersistentnet.run())
+        self.assertTrue("[SUCCESS] /etc/udev/rules.d/70-persistent-net.rules not present."
+                        in self.output.getvalue())
+        self.assertTrue(detect_mock.called)
+        self.assertTrue(get_config_dict_mock.called)
+
+    @mock.patch("moduletests.src.udevpersistentnet.get_config_dict")
+    @mock.patch("moduletests.src.udevpersistentnet.detect", return_value=True)
+    @mock.patch("moduletests.src.udevpersistentnet.backup", return_value=True)
+    @mock.patch("moduletests.src.udevpersistentnet.fix", side_effect=IOError())
+    @mock.patch("moduletests.src.udevpersistentnet.restore", return_value=True)
+    def test_run_failure(self, restore_mock, fix_mock, backup_mock, detect_mock, get_config_dict_mock):
+        get_config_dict_mock.return_value = {"BACKUP_DIR": "/var/tmp/ec2rl",
+                                             "LOG_DIR": "/var/tmp/ec2rl",
+                                             "BACKED_FILES": {self.rules_file: "/some/path"},
+                                             "REMEDIATE": True}
+        with contextlib.redirect_stdout(self.output):
+            self.assertFalse(moduletests.src.udevpersistentnet.run())
+        self.assertTrue(self.output.getvalue().endswith("Review the logs to determine the cause of the issue.\n"))
+        self.assertTrue(restore_mock.called)
+        self.assertTrue(fix_mock.called)
+        self.assertTrue(backup_mock.called)
+        self.assertTrue(detect_mock.called)
+        self.assertTrue(get_config_dict_mock.called)
+
+    @mock.patch("moduletests.src.udevpersistentnet.get_config_dict", side_effect=Exception)
+    def test_run_config_exception(self, get_config_dict_mock):
+        with contextlib.redirect_stdout(self.output):
+            self.assertFalse(moduletests.src.udevpersistentnet.run())
+        self.assertTrue(self.output.getvalue().endswith("Review the logs to determine the cause of the issue.\n"))
+        self.assertTrue(get_config_dict_mock.called)
